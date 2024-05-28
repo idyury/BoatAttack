eabf2f018f18bb89e6fb8f0630451381e67e1024
Yury Khmel <khmel@google.com>
2024-05-28 11:05:00 -0700

    fixed fps 60

--- Assets/Scripts/Utility/DemoRun.cs
+++ Assets/Scripts/Utility/DemoRun.cs
@@ -6,6 +6,8 @@ public class DemoRun : MonoBehaviour
 {
     private void Start()
     {
+        Application.targetFrameRate = 60;
+        Debug.Log($"Set targetFrameRate {Application.targetFrameRate}. Resolution {Screen.currentResolution}");
         StartCoroutine(RaceManager.SetupRace());
     }
 }
